be9d1888c3074598771b00c68fec40d39637be17
vitorino99 <vitorcamilolemos@gmail.com>
2024-12-07 02:56:54 -0300

    Solução otimizada para o problema

--- day1/part1.py
+++ day1/part1.py
@@ -6,20 +6,16 @@ with open("AdventOfCode/day1/input.txt", 'r') as file:
     for line in file:
         valores = line.split()
         if len(valores) == 2:
-            coluna2.append(int(valores[0]))  
-            coluna1.append(int(valores[1]))  
-            
+            coluna2.append(int(valores[0]))
+            coluna1.append(int(valores[1]))
+    coluna1.sort()
+    coluna2.sort()
+
 resultado = 0
 tamanho_lista1 = len(coluna1)
 tamanho_lista2 = len(coluna2)
 
 for x in range(0,tamanho_lista1):
-    menor_valor_lista_1 = min(coluna1)
-    menor_valor_lista_2 = min(coluna2)
-    resultado += abs(menor_valor_lista_1 - menor_valor_lista_2)
-    coluna1.remove(menor_valor_lista_1)
-    coluna2.remove(menor_valor_lista_2)
+    resultado += abs(coluna1[x] - coluna2[x])
 
 print(resultado)
-    
-    
