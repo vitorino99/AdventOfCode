0e0708f46b355184bae59fdca07f5b06999112bc
vitorino99 <vitorcamilolemos@gmail.com>
2024-12-07 02:48:16 -0300

    Correção no caminho de leitura do arquivo

--- day1/part1.py
+++ day1/part1.py
@@ -2,7 +2,7 @@ coluna1 = []
 coluna2 = []
 
 
-with open("input.txt", 'r') as file:
+with open("AdventOfCode/day1/input.txt", 'r') as file:
     for line in file:
         valores = line.split()
         if len(valores) == 2:
