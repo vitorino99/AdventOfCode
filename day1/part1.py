644065f07c40db7ff8d2cc0ffcb80c1ff03ca842
vitorino99 <vitorcamilolemos@gmail.com>
2024-12-07 18:09:49 -0300

    Remove variavel não utilizada

--- day1/part1.py
+++ day1/part1.py
@@ -3,13 +3,9 @@ from utils import ler_arquivo_txt
 
 coluna1, coluna2 = ler_arquivo_txt('day1/input.txt')
 coluna1.sort(),coluna2.sort()
-
-
 resultado = 0
-tamanho_lista1 = len(coluna1)
-tamanho_lista2 = len(coluna2)
 
-for x in range(0,tamanho_lista1):
+for x in range(0,len(coluna1)):
     resultado += abs(coluna1[x] - coluna2[x])
 
 print(resultado)
