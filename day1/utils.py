5ade53e7fb1eb92e3c13acd31c611bfd4570e6ff
vitorino99 <vitorcamilolemos@gmail.com>
2024-12-19 12:41:37 -0300

    Redução de linhas na leitura de arquivos

--- day1/utils.py
+++ day1/utils.py
@@ -1,11 +1,7 @@
 def ler_arquivo_txt(caminho):
-    lista1 = []
-    lista2 = [] 
+    lista1, lista2 = [],[]
     with open(caminho, 'r') as file:
         for line in file:
             valores = line.split()
-            if len(valores) == 2:
-                lista1.append(int(valores[0]))
-                lista2.append(int(valores[1]))
-    
+            lista1.append(int(valores[0])), lista2.append(int(valores[1]))
     return lista1,lista2
